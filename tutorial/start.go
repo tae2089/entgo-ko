716ca26e9666db85432e1e1c90761ebb88086f33
tae2089 <tae2089002@gmail.com>
2022-03-19 20:32:39 +0900

    FEATURE: User 생성
    User를 생성합니다.

---
+++ tutorial/start.go
@@ -0,0 +1,21 @@
+package tutorial
+
+import (
+	"context"
+	"entgo-ko/ent"
+	"fmt"
+	"log"
+)
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
